403ed41e8c7e07984c6d815a819b79b9dd569f06
Ravi vema <85781883+Mickyverma24@users.noreply.github.com>
2023-11-17 15:57:34 +0530

    Update directimplement.cpp
    Methods and declaration of any ordered set

--- directimplement.cpp
+++ directimplement.cpp
@@ -4,3 +4,13 @@
 
 using namespace std;
 using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+int main(){
+{
+    pbds s;
+    // this is ordered set of integers which contians unique values in ascending order
+    // All the methods are same as set container of stl
+    // two extra useful methods are
+    auto iter = s.find_by_order(any_index);// finds element on index o(log n) // return an iterator
+    int less_than = s.order_of_key(any key);// return number of elements smaller than given key
+}
